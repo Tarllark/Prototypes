dc15c0338083c2b6cb25c165a02bc31fb7a7a5d3
Tarllark <33085010+Tarllark@users.noreply.github.com>
2019-02-13 11:02:10 +0100

    Moved project

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject UI;
+    [SerializeField]
+    GameObject plane;
+
+    GameObject coin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coin = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        UI.transform.Find("counter").GetComponent<Text>().text = (int.Parse(UI.transform.Find("counter").GetComponent<Text>().text) + 1).ToString();
+        Destroy(coin);
+    }
+
+}
---
+++ Assets/Scripts/Reset.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    [SerializeField]
+    GameObject car;
+    [SerializeField]
+    Transform respawnPoint;
+
+    public void ResetCar()
+    {
+        car.transform.position = respawnPoint.transform.position;
+    }
+}
